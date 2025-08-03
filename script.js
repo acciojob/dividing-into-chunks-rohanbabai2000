bf21c297349ba72c76fe1ed0ba1d1bff64e447af
ROHAN DAS <61402717+rohanbabai2000@users.noreply.github.com>
2025-08-03 19:13:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let s=0;
+	for(let i=0;i<arr.length;i++){
+		if(i>0 && s<=n){
+			s=s+i;
+			return true;
+			
+		}
+	}
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+//const n = prompt("Enter n: ");
+//alert(JSON.stringify(divide(arr, n)));
